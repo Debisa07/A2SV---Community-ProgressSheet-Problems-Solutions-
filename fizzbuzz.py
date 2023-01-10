f67bde92bb2e568756fb73a8608e8e86690e8dbc
Debisa Abebe/SA <57017005+Debisa07@users.noreply.github.com>
2023-01-10 21:07:20 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,22 @@
 
-n = int(input("enter N: "))
-fizbuzz = []
-for i in range(1,n+1):
-    if x%15 == 0:
-        fizbuzz.append("FizzBuzz")
-    elif x % 5 == 0:
-        fizbuzz.append("Buzz")
-    elif x % 3 == 0:
-        fizbuzz.append(Fizz)
-return fizbuzz
 
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ret = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                ret.append('FizzBuzz')
+            elif i % 3 == 0:
+                ret.append('Fizz')
+            elif i % 5 == 0:
+                ret.append('Buzz')
+            else:
+                ret.append(str(i))
+        return ret
 
 
 
